946e45bc57c696ab5e3068eadcd010d6fe3b7706
randymcmillan <randymcmillan@protonmail.com>
2026-03-08 22:25:21 -0400

    src/lib/p2p.rs:intermediate

--- src/bin/p2p_node.rs
+++ src/bin/p2p_node.rs
@@ -1,4 +1,15 @@
+use byz_time::p2p::{evt_loop, InternalEvent, MsgKind, Msg, IPFS_PROTO_NAME};
+use libp2p::gossipsub::IdentTopic;
+use anyhow::Result;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    byz_time::p2p::start_p2p_node().await
+    // Dummy channels for compilation
+    let (send_tx, send_rx) = tokio::sync::mpsc::channel(100);
+    let (recv_tx, recv_rx) = tokio::sync::mpsc::channel(100);
+
+    // Dummy topic for compilation
+    let topic = IdentTopic::new("dummy-topic");
+
+    evt_loop(send_rx, recv_tx, topic).await.map_err(|e| Box::new(e) as Box<dyn std::error::Error>)
 }
